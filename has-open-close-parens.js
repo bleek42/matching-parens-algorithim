7153d6e3018c452ddb3e0d8bc9cb3b141beb5cfb
bleek42 <brandonleek42@gmail.com>
2020-06-08 00:04:33 -0400

    really found a solution: parensPairsInString(), line 40

--- has-open-close-parens.js
+++ has-open-close-parens.js
@@ -1,16 +1,25 @@
-function matchingParens(str) {
+/* eslint-disable strict */
+function stackParens(str) {
+  if (typeof str !== 'string' && !str.includes('(' || ')')) {
+    throw TypeError(
+      'invalid input: alphanumeric string with at least one parentheses only!'
+    );
+  }
   let stack = [];
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === '(' || ')') {
+    if (str[i] === '(' || str[i] === ')') {
       stack.push(str[i]);
     }
   }
   return stack;
 }
 
-matchingParens('(jebcjfj)');
-
 function checkOpenCloseParens(str) {
+  if (typeof str !== 'string' && !str.includes('(' || ')')) {
+    throw TypeError(
+      'invalid input: alphanumeric string with at least one parentheses only!'
+    );
+  }
   let opens = 0;
   let closes = 0;
   for (let i = 0; i < str.length; i++) {
@@ -27,23 +36,33 @@ function checkOpenCloseParens(str) {
   }
 }
 
-function parensPairs(str) {
-  if(typeof str !== 'string') {
-    alert('invalid input type: alphanumeric strings only!')
+// this one works, but can't seem to get it to work with a stack([]) instead of counter.
+function parensPairsInString(str) {
+  if (typeof str !== 'string' && !str.includes('(' || ')')) {
+    throw TypeError(
+      'invalid input: alphanumeric string with at least one parentheses only!'
+    );
   }
-  //let stack = [];
-  const count = 0;
-  for(let i = 0; i < str.length; i++) {
-    if(str[i] === '(') {
+  // init a counter to track open/close parens of a string
+  let count = 0;
+  // loop over our argument string
+  for (let i = 0; i < str.length; i++) {
+    // check each char in string for open parens
+    if (str[i] === '(') {
+      // add by 1 for each open parens found
       count++;
-    } else if(str[i] === ')') {
+      // now check for close parens
+    } else if (str[i] === ')') {
+      // remove by 1 for each close parens found
       count--;
     }
   }
-  if(count < 0 || count <= -1) {
+  // check if counter value didn't balance back to 0
+  if (count < 0 || count <= -1) {
     return false;
   }
+  // all condtions passed: input string has open '(' for every ')' within
   return true;
 }
 
-console.log(checkOpenCloseParens('((hevdhueb)hebxh)'));
+console.log(parensPairsInString('((hevdhueb)hebxh)'));
